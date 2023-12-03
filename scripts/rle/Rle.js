731e9c8d66ef128dd7c7d54d04d491492009508c
kystttt <146662933+kystttt@users.noreply.github.com>
2023-12-03 16:54:48 +0500

    Update Rle.js

--- scripts/rle/Rle.js
+++ scripts/rle/Rle.js
@@ -1,49 +1,61 @@
 let fs = require('fs');
-let inText, outText,uncText;
+let inText, outText, uncText;
 let i = 0, n = 1;
-let bufer = ""
-
-
+let bufer = "";
+let actionn = (process.argv[2]);
 inText = fs.readFileSync('input.txt');
 inText = inText.toString();
-console.log(inText);
 
 
 
-while (i < inText.length){
-		while (inText.charAt(i) == inText.charAt(i + n))
-			n++;
-		console.log(inText.charAt(i), " - ", n)
-		i += n;
-		
-		if ((n > 3) || (inText.charAt(i-1) == '#')){
-		bufer += ("#" + String.fromCharCode(255) + inText.charAt(i-1)).repeat(Math.floor(n / 255));
-		bufer += "#" + String.fromCharCode(n % 255) + inText.charAt(i-1)
-		}
-		else {
-		bufer += inText.charAt(i-1).repeat(n);
-		}
-		n = 1;
+	switch (actionn) { //через кейсы делаем разветвление, кодируем или декодируем мы
+
+		case 'code': // кодируем наш текст
+			while (i < inText.length) {
+				while (inText.charAt(i) == inText.charAt(i + n))
+					n++;
+				i += n;
+
+				if ((n > 3) || (inText.charAt(i - 1) == '#')) {
+					bufer += ("#" + String.fromCharCode(255) + inText.charAt(i - 1)).repeat(Math.floor(n / 255));
+					bufer += "#" + String.fromCharCode(n % 255) + inText.charAt(i - 1)
+				}
+				else {
+					bufer += inText.charAt(i - 1).repeat(n);
+				}
+				n = 1;
+			}
+			//добавляем закодированное сообщение в файл code.txt
+			fs.writeFileSync('code.txt', bufer);
+			outText = fs.readFileSync('code.txt');
+			outText = outText.toString();
+			break;
+
+
+		case 'decode':
+
+			//декодируем наш текст
+			let cText = fs.readFileSync('code.txt');
+			cText = cText.toString();
+			let unbufer;
+			unbufer = ""
+			for (let j = 0; j < cText.length; j++) {
+				if (cText.charAt(j) == "#") {
+					unbufer += cText.charAt(j + 2).repeat(cText.charCodeAt(j + 1));
+					j += 2;
+				}
+				else {
+					unbufer += cText.charAt(j);
+				}
+			}
+			// добавляем декодированное сообщение в файл decode.txt
+			fs.writeFileSync('decode.txt', unbufer);
+			uncText = fs.readFileSync('decode.txt');
+			uncText = uncText.toString();
+			console.log(inText.length / cText.length); // вычисляем коэф сжатия
+			break;
+
+		default:
+			console.log('Error, invalid action');
+			break;
 	}
-//write our code string in cmd
-fs.writeFileSync('code.txt', bufer);
-outText = fs.readFileSync('code.txt');
-outText = outText.toString();
-console.log(outText);
-
-//uncode
-let unbufer;
-unbufer = ""
-for (let j = 0; j  < outText.length; j++) {
-	if (outText.charAt(j) == "#") {
-		unbufer += outText.charAt(j + 2).repeat(outText.charCodeAt(j+1));
-		j += 2;
-		}
-	else {
-		unbufer += outText.charAt(j);
-	}
-}
-fs.writeFileSync('decode.txt', unbufer);
-uncText = fs.readFileSync('decode.txt');
-uncText = uncText.toString();
-console.log(uncText);
