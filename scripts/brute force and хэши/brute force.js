a331ed6e9773b952e3981b7b36490a5f7270c60d
Leonid <146662933+kystttt@users.noreply.github.com>
2024-01-25 04:37:55 +0500

    brute force.js

--- scripts/brute force and хэши/brute force.js
+++ scripts/brute force and хэши/brute force.js
@@ -1,1 +1,21 @@
+let S = 'abbbfbbb';
+let T = 'bb'; //1 2 5 6
+let n = S.length, m = T.length;
+let answer = new Array();
+let i, j;
 
+i = 0;
+while (i <= n - m) {
+    j = 0;
+    while (S[i + j] == T[j]) {
+        j++;
+        
+        if (j == m) {
+            answer.push(i);
+            break;
+        }
+    }
+    i++;
+}
+
+console.log(answer);
